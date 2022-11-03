2528e38ae77ce074b0d79f984370332af47a0572
David Dana <davdancab@alum.us.es>
2022-11-03 17:28:03 +0100

    primer commit

--- src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoomRepository.java
+++ src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoomRepository.java
@@ -3,10 +3,22 @@ package org.springframework.samples.petclinic.recoveryroom;
 import java.util.List;
 import java.util.Optional;
 
-public interface RecoveryRoomRepository {
-    List<RecoveryRoom> findAll();
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+public interface RecoveryRoomRepository extends CrudRepository<RecoveryRoom, Integer>{
+    
+	@Query("SELECT recoveryRoom FROM RecoveryRoom recoveryRoom")
+	List<RecoveryRoom> findAll();
     Optional<RecoveryRoom> findById(int id);
-    RecoveryRoom save(RecoveryRoom p);
-    //List<RecoveryRoomType> findAllRecoveryRoomTypes();
-    //RecoveryRoomType getRecoveryRoomType(String name);
+    
+    @Query("SELECT recoveryRoomType FROM RecoveryRoomType recoveryRoomType")
+    List<RecoveryRoomType> findAllRecoveryRoomTypes();
+    
+    @Query("SELECT recoveryRoomType FROM RecoveryRoomType recoveryRoomType WHERE recoveryRoomType.name =:name")
+    RecoveryRoomType getRecoveryRoomType(@Param("name") String name);
+    
+    @Query("SELECT recoveryRoom FROM RecoveryRoom recoveryRoom WHERE recoveryRoom.roomType LIKE :roomType%")
+    List<RecoveryRoom> getRecoveryRoomsByType(@Param("roomType") RecoveryRoomType type);
 }
